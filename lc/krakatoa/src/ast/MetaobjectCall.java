1806ad7b2fbec7ac36c357817c4ee70d12b37c02
vcasadei <vitor.casadei@gmail.com>
2015-10-14 10:39:17 -0300

    More headers

--- lc/krakatoa/src/ast/MetaobjectCall.java
+++ lc/krakatoa/src/ast/MetaobjectCall.java
@@ -1,5 +1,11 @@
 /**
-
+ * Laboratório de Compiladores [2015/2] <br>
+ * Orientação: Prof. Dr. José de O. Guimarães <br>
+ * 
+ * @author Maurício Spinardi | 401874 <br>
+ * @author Vitor Casadei | 408301 <br>
+ * 
+ * @see http://www.cyan-lang.org/jose/courses/15-2/lc/index.htm
  */
 package ast;
 
